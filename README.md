769c1444e0848337f31ce3a92853b286cf46bb3f
Bindu Wavell <github@spamguard.wavell.net>
2025-02-02 21:06:09 -0700

    Very basic CLI

---
+++ README.md
@@ -0,0 +1,7 @@
+# KeyBard CLI
+
+## Devbox
+
+Devbox creates isolated, reproducible development environments that run anywhere. No Docker containers or Nix lang required.
+
+For this project, we want specific versions of Node. Additionally, we want to make sure that our keyboard is accessible via USB.
---
+++ cli.js
@@ -0,0 +1,72 @@
+const { Blob } = require('buffer');
+const debug = require('debug')('keybard-cli:cli');
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
+const vm = require('vm');
+const { XzReadableStream } = require('xz-decompress');
+
+
+const sandbox = {
+  global: {},
+  Blob: Blob,
+  console: console,
+  localStorage: {
+    data: {},
+    getItem: function(key) {
+      if (this.data.hasOwnProperty(key)) {
+        return this.data[key];
+      } else {
+        return null;
+      }
+    },
+    setItem: function(key, val) {
+      this.data[key] = val;
+    }
+  },
+  Response: Response,
+  require: require,
+  TextDecoder: TextDecoder,
+  xzwasm: {XzReadableStream: XzReadableStream}
+}
+vm.createContext(sandbox);
+
+const files = [
+    'keybard/pages/js/util.js',
+    'keybard/pages/js/svalboard.js',
+    'keybard/pages/js/keygen.js',
+    'keybard/pages/js/languages.js',
+    'keybard/pages/js/keys.js',
+    'keybard/pages/js/jskeys.js',
+    'keybard/pages/js/kbinfo.js',
+    'lib/node_usbhid.js',
+    'keybard/pages/js/qmk_settings.js',
+    'keybard/pages/js/kle.js',
+    'keybard/pages/js/vial/init.js',
+    'keybard/pages/js/vial/usb.js',
+    'keybard/pages/js/vial/kb.js',
+    'keybard/pages/js/vial/macro.js',
+    'keybard/pages/js/vial/combo.js',
+    'keybard/pages/js/vial/tapdance.js',
+    'keybard/pages/js/vial/keyoverride.js',
+    'keybard/pages/js/vial/qmk.js',
+    'keybard/pages/js/vial/sval.js',
+    'keybard/pages/js/vial/api.js',
+    'keybard/pages/js/vial/vial.js',
+];
+files.forEach((file) => {
+  debug('Loading ' + file + '.');
+  const js = fs.readFileSync(path.resolve(__dirname, file), 'utf8');
+  vm.runInContext(js, sandbox);
+});
+
+program.command('dump').action(() => {
+  const js = fs.readFileSync(path.resolve(__dirname, 'lib/dump.js'), 'utf8');
+  vm.runInContext(js, sandbox);
+});
+
+program.command('list').action(() => {
+  vm.runInContext('USB.list();', sandbox);
+});
+
+program.parse(process.argv);
